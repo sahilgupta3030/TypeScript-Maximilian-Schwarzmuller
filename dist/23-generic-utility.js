899ced8617c5112b243c1e1333fdd62fdc1f1518
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-11-22 15:12:44 +0530

    learnt generic utility - Partial and Readonly

---
+++ dist/23-generic-utility.js
@@ -0,0 +1,24 @@
+"use strict";
+// // // // Utility types......
+// // // Partial..
+// interface CourseGoal {
+//     title: string;
+//     description: string;
+//     completeUntil: Date;
+// }
+// function createCourseGoal(
+//     title: string,
+//     description: string,
+//     date: Date
+// ): CourseGoal {
+//     // here used partially...
+//     let courseGoal: Partial<CourseGoal> = {};
+//     courseGoal.title = title;
+//     courseGoal.description = description;
+//     courseGoal.completeUntil = date;
+//     return courseGoal as CourseGoal;
+// }
+// // // Readonly..
+// const names: Readonly<string[]> = ['Chandler', 'Joey'];
+// // names.push('Ross'); // Not working
+// // names.pop(); // Not working
